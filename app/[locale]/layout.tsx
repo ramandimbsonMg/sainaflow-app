0634e0ac136e3f434b8538654e39160d413656f4
ramandimbsonMg <teresperanto@gmail.com>
2025-07-08 14:00:00 +0200

    feat: add root layout with Inter and Poppins fonts

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,81 @@
+import "./globals.css";
+
+import { Inter, Poppins } from "next/font/google";
+
+import { ReactNode } from "react";
+
+import { NextIntlClientProvider } from "next-intl";
+import { getTranslations, getMessages } from "next-intl/server";
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/app/providers/ThemeProvider";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+type Props = {
+  children: ReactNode;
+  params: Promise<{ locale: string }>;
+};
+
+export async function generateMetadata(props: Props) {
+  const params = await props.params;
+
+  const { locale } = params;
+
+  const t = await getTranslations({ locale, namespace: "RootLayout" });
+
+  return {
+    metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
+    title: t("title"),
+    description: t("description"),
+    openGraph: {
+      images: [
+        {
+          url: "/images/opengraph-image.png",
+          width: 1200,
+          height: 630,
+          alt: t("title"),
+        },
+      ],
+    },
+    twitter: {
+      cardType: "summary_large_image",
+      image: "/images/opengraph-image.png",
+      width: 1200,
+      height: 630,
+      alt: t("title"),
+    },
+  };
+}
+
+export default async function RootLayout(props: Props) {
+  const params = await props.params;
+
+  const { locale } = params;
+
+  const { children } = props;
+
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body
+        className={`${inter.variable} ${poppins.variable} font-sans min-h-screen`}
+      >
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </NextIntlClientProvider>
+        <Toaster />
+        <SonnerToaster />
+      </body>
+    </html>
+  );
+}
